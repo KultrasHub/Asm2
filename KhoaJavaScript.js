81963725a80083ac790d66b9848f5f89b7ce6c2e
kultras <kultras@kultrass-MacBook-Pro.local>
2020-08-14 17:03:39 +0700

    Selection available

---
+++ KhoaJavaScript.js
@@ -0,0 +1,40 @@
+function Select(index)
+{
+  if(index==1)
+  {
+    document.getElementById('NameTextID').innerHTML="Tran Nguyen Anh Khoa";
+    document.getElementById('NameTextID').style.color="#d4d8fa";
+    document.getElementById('DisplayID').style.backgroundImage="url('KhoaPose.jpg')";
+    document.getElementById('GradientLineID').style.backgroundImage='linear-gradient(to right,#d4d8fa, #000)';
+    document.getElementById('StudentID').innerHTML="ID: S3863956";
+    document.getElementById('ContentID').innerHTML="Born and raised in a tranquill city, Dalat, I don't usually lose my temper. I could stay calm in most situations and don't really care what people think of me. However, I am always a curious boy, I often fantasize about living in a sci-fi worlds and visual worlds. I love watching movies of the fantasy future and hold the desire of creating my own imaginary world where people enjoy. <br> <br> I have a great enthusiasm and interest in space and cartoon, for that, I am extremely into sci-fi tales from movies or books. Additionally, I consider myself a crafty person, I made dragons, soliders out of paper because it fulfilled my desire. However, papercrafting seems a bit off for a 18 years old boy so I changed to game development. To be honest, I love making video games, when I first started it with unity I realise what my purpose is. I have over twenty projects, though the majority of them is abandoned I completed some. Then I receive many compliments of the games but I don't consider it a success. Therefore, I will still try at least till I get the achievement that satisfy me.";
+  }
+  if(index==2)
+  {
+    document.getElementById('NameTextID').innerHTML="Khang";
+    document.getElementById('NameTextID').style.color="#ea613b";
+    document.getElementById('DisplayID').style.backgroundImage="url('KentPose.jpg')";
+    document.getElementById('GradientLineID').style.backgroundImage='linear-gradient(to right,#ea613b, #000)';
+    document.getElementById('StudentID').innerHTML="ID: S.....";
+    document.getElementById('ContentID').innerHTML="Hi Im Khang";
+  }
+  if(index==3)
+  {
+    document.getElementById('NameTextID').innerHTML="Phong";
+    document.getElementById('NameTextID').style.color="#8ddc48";
+    document.getElementById('DisplayID').style.backgroundImage="url('PhongPose.jpg')";
+    document.getElementById('GradientLineID').style.backgroundImage='linear-gradient(to right,#d4d8fa, #000)';
+    document.getElementById('StudentID').innerHTML="ID: S.....";
+    document.getElementById('ContentID').innerHTML="hi im Phong";
+  }
+  if(index==4)
+  {
+    document.getElementById('NameTextID').innerHTML="Nguyen Hoang Minh Nhat";
+    document.getElementById('NameTextID').style.color="#f4c6d6";
+    document.getElementById('DisplayID').style.backgroundImage="url('NhatPose.jpg')";
+    document.getElementById('GradientLineID').style.backgroundImage='linear-gradient(to right,#f4c6d6, #000)';
+    document.getElementById('StudentID').innerHTML="ID: S3765963";
+    document.getElementById('ContentID').innerHTML=" I am Vietnamese.Then, just a little bit about myself that I want to write down, that is I am not an introvert. I am lazy whenever nothing can interest me to do but as soon as I like it, I spend all the time for it, days and nights. I like playing badminton and soccer, but I do not have time and partners to play with, I can be in any group and type of people. There is a long story for me about absolutely studying Information Technology. Since I was in grade 11th, I wished that someday I could create something innovative just like Apple or Microsoft. First it was a dream, then my brother said that IT is one of the most difficult majors in which students graduate very few. At that moment, I was scared, I listened to my parents apply into Hospitality in RMIT. Right there, after two semesters studying, I had found myself getting bored because of business. I found Daniel Shiffman channel who teaches JS, HTML, CSS, ect,.. I admired him and started learning code from him and Processing was the first application which I wrote down my first code. Now, I am going to be a Junior then further due to learning all courses in RMIT. I am wondering if RMIT or me or could be both will help me to be good at using code to help people. ";
+  }
+
+}
